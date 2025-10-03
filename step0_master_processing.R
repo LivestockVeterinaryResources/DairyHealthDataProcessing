fc393c03c8c368e9dc3b38bc150f3509df01e8fd
Gerard Cramer <gcramer@umn.edu>
2025-10-03 15:35:56 -0500

    style and remove long format processing

--- step0_master_processing.R
+++ step0_master_processing.R
@@ -7,86 +7,80 @@ pacman::p_load(
   arrow,
   rmarkdown,
   lubridate,
-  quarto, 
+  quarto,
   arrow,
   zoo,
   DT
-  )
+)
 
-#read in functions -------------------
-source('functions/fxn_parse_free_text.R') #functions to parse remarks and protocols
-source('functions/fxn_event_type.R') #c function to categorize events
-source('functions/fxn_location.R') #function to specify event location
+# read in functions -------------------
+source("functions/fxn_parse_free_text.R") # functions to parse remarks and protocols
+source("functions/fxn_event_type.R") # c function to categorize events
+source("functions/fxn_location.R") # function to specify event location
 
-#set custom functions----
-#****Modify This Section ***
-fxn_parse_remark<-fxn_parse_remark_default # parse_free_text options: fxn_parse_remark_default, fxn_parse_remark_custom
+# set custom functions----
+#**** Modify This Section ***
+fxn_parse_remark <- fxn_parse_remark_default # parse_free_text options: fxn_parse_remark_default, fxn_parse_remark_custom
 
-fxn_parse_protocols<-fxn_parse_protocols_default #parse_free_text options: fxn_parse_protocols_default, fxn_parse_protocols_custom
+fxn_parse_protocols <- fxn_parse_protocols_default # parse_free_text options: fxn_parse_protocols_default, fxn_parse_protocols_custom
 
-fxn_assign_location_event<-fxn_assign_location_event_parnell_ANON #location_event options: fxn_assign_location_event_default, fxn_assign_location_event_custom
+fxn_assign_location_event <- fxn_assign_location_event_parnell_ANON # location_event options: fxn_assign_location_event_default, fxn_assign_location_event_custom
 
-fxn_event_type<-fxn_assign_event_type_default #event_type options: fxn_assign_event_type_default, fxn_assign_event_type_custom
+fxn_event_type <- fxn_assign_event_type_default # event_type options: fxn_assign_event_type_default, fxn_assign_event_type_custom
 
-fxn_detect_location_lesion<-fxn_detect_location_lesion_default #detect_location_lesion options: fxn_detect_location_lesion_default, fxn_detect_location_lesion_custom
+fxn_detect_location_lesion <- fxn_detect_location_lesion_default # detect_location_lesion options: fxn_detect_location_lesion_default, fxn_detect_location_lesion_custom
 
-#Set up -------------------------------
-#****Modify This Section***
+# Set up -------------------------------
+#**** Modify This Section***
 
-event_data_exists<-TRUE
-milk_data_exists<-FALSE
+event_data_exists <- TRUE
+milk_data_exists <- FALSE
 
-set_farm_name<-'demo'
+set_farm_name <- "demo"
 
-#Step 1 - Process files--------------------------
+# Step 1 - Process files--------------------------
 
 ## proces event data -----------------
-if (event_data_exists==TRUE){
-source('step1_read_in_data.R')
+if (event_data_exists == TRUE) {
+  source("step1_read_in_data.R")
 }
 
 ## process milk data ---------------------
-if (milk_data_exists==TRUE){
-source('step1a_read_in_production_data.R')
+if (milk_data_exists == TRUE) {
+  source("step1a_read_in_production_data.R")
 }
 
 ### Step 2 Intermediate Files----------------------
-#***Modify This Step to Include the Events/Disease of Interest***
+#*** Modify This Step to Include the Events/Disease of Interest***
 #*
 #* Set Gaps
 
-#*set this to be the number of days between events that would still count as
+#* set this to be the number of days between events that would still count as
 #* the same event
-set_outcome_gap_animal<- 1 
-#* set this to be the number of days between events in lactation 
+set_outcome_gap_animal <- 1
+#* set this to be the number of days between events in lactation
 #* that would still count as the same event
-set_outcome_gap_lactation<- 1 
+set_outcome_gap_lactation <- 1
 
-#* set events of interest to create long and wide disease specifics data sets
-#* #***Modify this *** to be the list of events you want to explore
-list_selected_events<-c('BRED') 
-
-#create intermediate files
-source('step2_create_intermediate_files.R')
+# create intermediate files
+source("step2_create_intermediate_files.R")
 
 # event check reports
-quarto::quarto_render('explore_event_types.qmd') 
-quarto::quarto_render('data_dictionary.qmd')
+quarto::quarto_render("explore_event_types.qmd")
+quarto::quarto_render("data_dictionary.qmd")
 
 ### Step3 Create Denominators ---------------------
-quarto::quarto_render('step3_create_denominators_lact_dim_season.qmd')
+quarto::quarto_render("step3_create_denominators_lact_dim_season.qmd")
 
 
 ### Step 4 Report Templates------------------------
-#add basic report templates
-#quarto::quarto_render('sara_Report_Template.qmd')
+# add basic report templates
+quarto::quarto_render("explore_lame.qmd")
 
 
 
 # disease report (under development)
-# quarto::quarto_render('step3_report_disease_template.qmd') 
-#cohort disease incidence (Location, Lactation, Breed, etc)
-#timing of disease (DIM (or Age) and calendar time distributions, Kaplan Meier)
-#perfomrance and disease (milk, gain, repro)
-
-
+# quarto::quarto_render('step3_report_disease_template.qmd')
+# cohort disease incidence (Location, Lactation, Breed, etc)
+# timing of disease (DIM (or Age) and calendar time distributions, Kaplan Meier)
+# perfomrance and disease (milk, gain, repro)
