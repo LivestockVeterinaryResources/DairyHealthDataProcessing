a894dd5b601c081f549fed1c02192eb1fa79b1fc
Gerard Cramer (work) <gerard@dairycowfootdoc.com>
2026-04-26 19:50:34 -0500

    fix extra )

--- functions/fxn_location.R
+++ functions/fxn_location.R
@@ -1,20 +1,19 @@
 library(tidyverse)
 
 
-#use herd id -------------------
-fxn_add_location_event <- function(df){
-  df%>%
+# use herd id -------------------
+fxn_add_location_event <- function(df) {
+  df %>%
     mutate(
       location_event = HERDID
-      )
     )
 }
 
 
-#use pen number -----------------------------
-fxn_assign_location_event_pen_template <- function(df){
-  df%>%
-    mutate(pen_num = parse_number(PEN))%>%
+# use pen number -----------------------------
+fxn_assign_location_event_pen_template <- function(df) {
+  df %>%
+    mutate(pen_num = parse_number(PEN)) %>%
     mutate(
       location_event = case_when(
         (pen_num == 0) ~ "Pen Zero"
@@ -27,39 +26,42 @@ fxn_assign_location_event_pen_template <- function(df){
 }
 
 
-#location from parenell source file ------------------------------
-fxn_assign_location_event_parnell_ANON <- function(df){
-  
-  df%>%
+# location from parenell source file ------------------------------
+fxn_assign_location_event_parnell_ANON <- function(df) {
+  df %>%
     mutate(
       location_event = paste0("Herd ", str_sub(source_file_path, 18, 22))
     )
 }
 
-#location lesion------------------
-fxn_detect_location_lesion<-function(df){
-  df%>%
+# location lesion------------------
+fxn_detect_location_lesion <- function(df) {
+  df %>%
     mutate(
       detectRR = case_when(
-        str_detect(Remark, 'RR|.RR|RR.|.RR.|RH|.RH|RH.|.RH.|ALL|.ALL|ALL.|.ALL.')~'RR', 
-        TRUE~''),
+        str_detect(Remark, "RR|.RR|RR.|.RR.|RH|.RH|RH.|.RH.|ALL|.ALL|ALL.|.ALL.") ~ "RR",
+        TRUE ~ ""
+      ),
       detectLR = case_when(
-        str_detect(Remark, 'LR|.LR|LR.|.LR.|LH|.LH|LH.|.LH.|ALL|.ALL|ALL.|.ALL.')~'LR', 
-        TRUE~''), 
+        str_detect(Remark, "LR|.LR|LR.|.LR.|LH|.LH|LH.|.LH.|ALL|.ALL|ALL.|.ALL.") ~ "LR",
+        TRUE ~ ""
+      ),
       detectRF = case_when(
-        str_detect(Remark, 'RF|.RF|RF.|.RF.|BF|.BF|BF.|.BF.|ALL|.ALL|ALL.|.ALL.')~'RF',
-        TRUE~''),
+        str_detect(Remark, "RF|.RF|RF.|.RF.|BF|.BF|BF.|.BF.|ALL|.ALL|ALL.|.ALL.") ~ "RF",
+        TRUE ~ ""
+      ),
       detectLF = case_when(
-        str_detect(Remark, 'LF|.LF|LF.|.LF.|BF|.BF|BF.|.BF.|ALL|.ALL|ALL.|.ALL.')~'LF',
-        TRUE~'')
-    )%>%
+        str_detect(Remark, "LF|.LF|LF.|.LF.|BF|.BF|BF.|.BF.|ALL|.ALL|ALL.|.ALL.") ~ "LF",
+        TRUE ~ ""
+      )
+    ) %>%
     mutate(locate_lesion = paste0(detectRR, detectLR, detectRF, detectLF))
 }
 
 
-#location lesion------------------
-fxn_detect_location_lesion_default<-function(df){
-  df%>%
+# location lesion------------------
+fxn_detect_location_lesion_default <- function(df) {
+  df %>%
     mutate(
       detectRR = case_when(
         str_detect(Remark, "RR|.RR|RR.|.RR.|RH|.RH|RH.|.RH.|ALL|.ALL|ALL.|.ALL.") ~ "RR",
