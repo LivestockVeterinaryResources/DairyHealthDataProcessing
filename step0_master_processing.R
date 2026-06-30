e923722961845065d4b6bfd70c50ee16090130fa
pgorden <pgorden@iastate.edu>
2026-06-30 15:19:41 -0500

    Jun 30v1

--- step0_master_processing.R
+++ step0_master_processing.R
@@ -3,15 +3,15 @@ source('functions/setup_default_processing_options.R') #default settings: 1 exam
 
 #**** Modify This Section*** turn on to over ride default processing options
 
-#clean_up_old_files <- FALSE # Use FALSE here for your own data, or after the first download of example data. This will delete any previously processed files as well as raw data in the event_files folder
+clean_up_old_files <- FALSE # Use FALSE here for your own data, or after the first download of example data. This will delete any previously processed files as well as raw data in the event_files folder
 
-#get_EXAMPLE_herds <- 0 # (0-8) ## number of Parnell Example herds you want to process.## if this is set to 0, you need to put your own data in the event_files folder
+get_EXAMPLE_herds <- 0 # (0-8) ## number of Parnell Example herds you want to process.## if this is set to 0, you need to put your own data in the event_files folder
 
-run_reports <-FALSE #make this false if you just want to reprocess base data
+run_reports <-TRUE #make this false if you just want to reprocess base data
 
-#milk_data_exists <- TRUE # are there files in the milk_files folder that you want to process?
+milk_data_exists <- TRUE # are there files in the milk_files folder that you want to process?
 
-#auto_de_duplicate <- FALSE # do you want to de-duplicate rows in the event files? # (choose FALSE if there are treatments that happen more than once daily that you want to capture)
+auto_de_duplicate <- TRUE # do you want to de-duplicate rows in the event files? # (choose FALSE if there are treatments that happen more than once daily that you want to capture)
 
 
 #********************************************************************************
